e7af5bd1b839ee2ea13d4798ed2431f31b39ded7
kinstar <707102127@qq.com>
2020-11-09 10:26:31 +0800

    no message

--- offer200/src/ListNode.java
+++ offer200/src/ListNode.java
@@ -1,9 +1,10 @@
-package PACKAGE_NAME;
-
 /**
  * @Author Jinshihong
  * @Date 2020/11/9 10:02
  * @Version 1.0
  */
 public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
 }
--- offer200/src/offer_06.java
+++ offer200/src/offer_06.java
@@ -1,9 +1,68 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
 /**
  * @Author Jinshihong
  * @Date 2020/11/9 10:00
  * @Version 1.0
+ *
+ *  * Definition for singly-linked list.
+ *  * public class ListNode {
+ *  *     int val;
+ *  *     ListNode next;
+ *  *     ListNode(int x) { val = x; }
+ *  * }
+ *
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+
+
+示例 1：
+
+输入：head = [1,3,2]
+输出：[2,3,1]
+
+示例 1：
+
+输入：head = [1,3,2]
+输出：[2,3,1]
+ 
+
+限制：
+
+0 <= 链表长度 <= 10000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
+
 public class offer_06 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] reversePrint(ListNode head) {
+        Stack<Integer> stack=new Stack<>();
+        ListNode node=head;
+        while(node!=null){
+            stack.push(node.val);
+            node=node.next;
+        }
+        int len=stack.size();
+        int [] result=new int[len];
+        for (int i=0;i<len;i++){
+            result[i]=stack.pop();
+        }
+
+
+
+
+        return result;
+    }
+
+
+
+
 }
