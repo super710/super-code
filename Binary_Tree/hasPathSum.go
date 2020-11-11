76274b3cf1af28d891e750f9db8d4789431088dc
No Such Person Found <765177543@qq.com>
2020-11-11 20:43:27 +0800

    leetcode-112
    hasPathSum

---
+++ Binary_Tree/hasPathSum.go
@@ -0,0 +1,15 @@
+package Binary_Tree
+
+
+/**
+思路很清晰，从根节点到叶子节点，因此递归终止的条件就是，sum==root.Val
+ */
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		return sum == root.Val
+	}
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
