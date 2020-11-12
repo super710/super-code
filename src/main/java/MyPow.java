3a87eb1def21db4b0657a2fa8106b0872522dba0
ROHAO <153510836@qq.com>
2020-11-12 22:30:34 +0800

    数值的整数次方

---
+++ src/main/java/MyPow.java
@@ -0,0 +1,28 @@
+/**
+ * 实现函数double Power(double base, int exponent)，求base的exponent次方。不得使用库函数，同时不需要考虑大数问题。
+ *
+ *题解：二分推倒
+ */
+public class MyPow {
+    public static  double myPow(double x, int n) {
+        if(x==0)
+            return 0;
+        long b = n;
+        double res =1.0;
+        if(b<0){
+            x = 1/x;
+            b =-b;
+        }
+
+        while(b>0){
+            if((b&1)==1) res*=x;
+            x*=x;
+            b>>=1;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myPow(2,-3));
+    }
+}
