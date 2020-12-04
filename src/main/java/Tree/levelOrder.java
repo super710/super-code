3079117dcf66a57e17364bdce69b18590b1a42b1
罗浩 <153510836@qq.com>
2020-12-04 12:30:20 +0800

    从上到下打印二叉树 三连

--- src/main/java/Tree/levelOrder.java
+++ src/main/java/Tree/levelOrder.java
@@ -99,6 +99,58 @@ public class levelOrder {
         return res;
     }
 
+
+    /**
+     * 请实现一个函数按照之字形顺序打印二叉树，
+     * 即第一行按照从左到右的顺序打印，
+     * 第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+     * 例如:
+     * 给定二叉树: [3,9,20,null,null,15,7],
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * 返回其层次遍历结果：
+     *
+     * [
+     *   [3],
+     *   [20,9],
+     *   [15,7]
+     * ]
+     *
+     *
+     */
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        int flag =1;//标记为0表示从左到右，1表示从右到左
+        List<List<Integer>> res = new ArrayList<>();//最终的数据列表
+        Queue<TreeNode> queue = new LinkedList<>();//bfs用消息队列来实现
+        if(root!=null){
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            //两个队列
+            LinkedList<Integer> list = new LinkedList<>();
+
+            for(int i =queue.size();i>0;i--) {
+                TreeNode node = queue.poll();
+                if (flag == 1) {
+                    list.addFirst(node.val);
+                } else {
+                    list.addLast(node.val);
+                }
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+            flag=flag == 1 ? 0 : 1 ;
+            res.add(list);
+        }
+            return  res;
+    }
+
     //前序遍历,找出节点个数，初始化
     public void preOrder(TreeNode node){
         if(node!=null){
